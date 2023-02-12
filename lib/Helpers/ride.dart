d18e70f28f5b19b71d8bc22227829e7098aa6ff1
Garvit Gulati <ggulati_be22@thapar.edu>
2023-02-12 12:46:06 +0530

    connected firebase

---
+++ lib/Helpers/ride.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/rendering.dart';
+import 'package:intl/intl.dart';
+
+class Ride {
+  bool? inProgress;
+  int? pointsEarned;
+  String? stationEnd;
+  String? stationStart;
+  String? timeStart;
+  String? timeEnd;
+  String? user;
+  DateTime? timeStartObj, timeEndObj;
+
+  void calculateTime() {
+    DateFormat dateFormat = DateFormat("yyyy-MM-dd hh:mm:ss");
+    timeStartObj = dateFormat.parse(timeStart!);
+    timeEndObj = dateFormat.parse(timeEnd!);
+  }
+}
--- lib/RideHistoryPage/history_page.dart
+++ lib/RideHistoryPage/history_page.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:velo/Helpers/theme.dart';
 import 'package:dotted_line/dotted_line.dart';
+
+import '../Helpers/ride.dart';
 
 class HistoryPage extends StatefulWidget {
   @override
@@ -10,15 +14,46 @@ class HistoryPage extends StatefulWidget {
 }
 
 class _HistoryPageState extends State<HistoryPage> {
+  var rides = <Ride>[];
+  @override
+  void initState() {
+    super.initState();
+    fetchRides().then((value) {
+      setState(() {});
+    });
+  }
+
+  Future<void> fetchRides() async {
+    final snap = await FirebaseDatabase.instance
+        .ref()
+        .child("Rides")
+        .child(FirebaseAuth.instance.currentUser!.uid)
+        .get();
+
+    for (final snapshot in snap.children) {
+      if (snapshot.child("inProgress").value == true) continue;
+      Ride ride = Ride();
+      ride.inProgress = snapshot.child("inProgress").value as bool?;
+      ride.stationStart = snapshot.child("stationStart").value.toString();
+      ride.stationEnd = snapshot.child("stationEnd").value.toString();
+      ride.timeStart = snapshot.child("timeStart").value.toString();
+      ride.timeEnd = snapshot.child("timeEnd").value.toString();
+      ride.user = snapshot.child("user").value.toString();
+      ride.calculateTime();
+      rides.add(ride);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(backgroundColor: kPrimaryColor),
       body: Container(
         child: ListView.builder(
-          itemCount: 20,
+          itemCount: rides.length,
           padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
           itemBuilder: (context, index) {
+            Ride ride = rides[index];
             return Card(
               clipBehavior: Clip.antiAlias,
               elevation: 4,
@@ -38,7 +73,7 @@ class _HistoryPageState extends State<HistoryPage> {
                               size: 10,
                               color: Colors.green,
                             ),
-                            Text(''),
+                            Text(ride.stationStart!),
                           ],
                         ),
                         SizedBox(
@@ -47,10 +82,15 @@ class _HistoryPageState extends State<HistoryPage> {
                             direction: Axis.vertical,
                           ),
                         ),
-                        Icon(
-                          Icons.circle,
-                          size: 10,
-                          color: Colors.red,
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.circle,
+                              size: 10,
+                              color: Colors.red,
+                            ),
+                            Text(ride.stationEnd!),
+                          ],
                         ),
                       ],
                     ),
